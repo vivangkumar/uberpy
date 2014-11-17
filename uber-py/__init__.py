4ed3a5600222756fce826dbe9fb409730b0174e7
Vivan <vivangkumar@gmail.com>
2014-11-17 20:31:24 +0000

    Add init.py

---
+++ uber-py/__init__.py
@@ -0,0 +1,14 @@
+__author__ = 'Vivan'
+__version__ = '1.0.0'
+
+'''
+Specify modules to be imported.
+'''
+
+import json
+
+try:
+    import httplib2
+except ImportError:
+    print "Please ensure that the httplib2 package is installed."
+
