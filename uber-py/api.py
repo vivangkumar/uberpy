7123f97a81499f466fd8c51fabfbc1e32c4cff11
vivangkumar <vivangkumar@gmail.com>
2014-11-18 14:39:06 +0000

    Add exceptions, API calls

--- uber-py/api.py
+++ uber-py/api.py
@@ -26,74 +26,70 @@ class Api(object):
         self.client = Http()
 
     def add_credentials(self, query_parameters):
-
         """
-        Adds the Uber credentials to the uri.
-        Params:
-            query_parameters: Takes the existing set of query parameters and adds the 'server_token' to it.
-        Returns:
-            query_parameters: Modified parameters with the credentials.
+        Adds the Uber server token to the query parameters to make an authorised request.
+        :param query_parameters: Query parameters to be sent.
+        :return: string
         """
-
         query_parameters['server_token'] = self.server_token
 
         return query_parameters
 
-    def sanitise_path(self, path):
-
+    @staticmethod
+    def sanitise_path(path):
         """
-        Ensure a preceding / in the path
-        Params:
-            path: The uri path to be checked for.
-        Returns:
-            path: Modified version of the path.
+        Adds a '/' to the path if it does not exist.
+        :param path: Path that is to be sanitised.
+        :return: string
         """
         if path[0] != '/':
             path = '/' + path
 
         return path
 
-    def check_status(self, uri, response):
+    @staticmethod
+    def check_status(content, response):
 
         """
-        Check the HTTP status codes for known errors that the Uber API may return.
-        Params:
-            uri: The uri of request.
-            response: Response returned by Uber API.
-        Returns:
-            None
+        Check the response that is returned for known exceptions and errors.
+        :param response: Response that is returned from the call.
+        :raise:
+         MalformedRequestException if `response.status` is 400
+         UnauthorisedException if `response.status` is 401
+         NotFoundException if `response.status` is 404
+         UnacceptableContentException if `response.status` is 406
+         InvalidRequestException if `response.status` is 422
+         RateLimitException if `response.status` is 429
+         ServerException if `response.status` > 500
         """
 
         if response.status == 400:
-            raise MalformedRequestException(uri, response)
+            raise MalformedRequestException(content, response)
 
         if response.status == 401:
-            raise UnauthorisedException(uri, response)
+            raise UnauthorisedException(content, response)
 
         if response.status == 404:
-            raise NotFoundException(uri, response)
+            raise NotFoundException(content, response)
 
         if response.status == 406:
-            raise UnacceptableContentException(uri, response)
+            raise UnacceptableContentException(content, response)
 
         if response.status == 422:
-            raise InvalidRequestException(uri, response)
+            raise InvalidRequestException(content, response)
 
         if response.status == 429:
-            raise RateLimitException(uri, response)
+            raise RateLimitException(content, response)
 
         if response.status >= 500:
-            raise ServerException(uri, response)
+            raise ServerException(content, response)
 
     def build_request(self, path, query_parameters):
-
         """
-        Construct URI for the API call.
-        Params:
-            path: The API end point to be used.
-            query_parameters: Parameters to be added to the query.
-        Returns:
-            url: The fully constructed URL
+        Build the HTTP request by adding query parameters to the path.
+        :param path: API endpoint/path to be used.
+        :param query_parameters: Query parameters to be added to the request.
+        :return: string
         """
         url = 'https://api.uber.com/v1' + self.sanitise_path(path)
         url += '?' + urlencode(query_parameters)
@@ -101,21 +97,21 @@ class Api(object):
         return url
 
     def get_json(self, uri_path, http_method='GET', query_parameters=None, body=None, headers=None):
-
         """
-        Make an API call and get the response returned in JSON.
-        Params:
-            uri_path: The endpoint to be used to make the request.
-            http_method: HTTP method to be used.
-            query_parameters: Parameters to be included in the request.
-            body: Optional body of the request.
-            headers: Optional headers to be added, if any.
+        Fetches the JSON returned, after making the call and checking for errors.
+        :param uri_path: Endpoint to be used to make a request.
+        :param http_method: HTTP method to be used.
+        :param query_parameters: Parameters to be added to the request.
+        :param body: Optional body, if required.
+        :param headers: Optional headers, if required.
+        :return: JSON
         """
         query_parameters = query_parameters or {}
         headers = headers or {}
 
         # Add credentials to the request
         query_parameters = self.add_credentials(query_parameters)
+
         # Build the request uri with parameters
         uri = self.build_request(uri_path, query_parameters)
 
@@ -123,6 +119,7 @@ class Api(object):
             headers['Content-Type'] = 'application/json'
 
         headers['Accept'] = 'application/json'
+        
         # Make the request
         response, content = self.client.request(
             uri=uri,
@@ -132,23 +129,6 @@ class Api(object):
         )
 
         # Check for known errors that could be returned
-        self.check_status(uri, response)
+        self.check_status(content, response)
 
         return json.loads(content.decode('utf-8'))
-
-    def products(self, latitude, longitude):
-        """
-        Lists all the Uber products available in an area.
-        Params:
-            latitude: Latitude coordinate for which the products are to be retrieved.
-            longitude: Longitude coordinate for which the products are to be retrieved.
-        Returns:
-            JSON returned by Uber
-        """
-        endpoint = 'products'
-        query_parameters = {
-            'latitude': latitude,
-            'longitude': longitude
-        }
-
-        return self.get_json(endpoint,'GET', query_parameters, None, None)
--- uber-py/uber.py
+++ uber-py/uber.py
@@ -12,16 +12,15 @@ class Uber(Api):
         self.client_id = client_id
         self.server_token = server_token
         self.secret = secret
+
         super(Uber, self).__init__(self.client_id, self.server_token, self.secret)
 
     def get_products(self, latitude, longitude):
         """
-        Lists all the Uber products available in an area.
-        Params:
-            latitude: Latitude coordinate for which the products are to be retrieved.
-            longitude: Longitude coordinate for which the products are to be retrieved.
-        Returns:
-            JSON returned by Uber
+        Get a list of all Uber products based on latitude and longitude coordinates.
+        :param latitude: Latitude for which product list is required.
+        :param longitude: Longitude for which product list is required.
+        :return: JSON
         """
         endpoint = 'products'
         query_parameters = {
@@ -30,3 +29,48 @@ class Uber(Api):
         }
 
         return self.get_json(endpoint,'GET', query_parameters, None, None)
+
+    def get_fare_estimate(self, start_latitude, start_longitude, end_latitude, end_longitude):
+        """
+        Returns the fare estimate based on two sets of coordinates.
+        :param start_latitude: Starting latitude or latitude of pickup address.
+        :param start_longitude: Starting longitude or longitude of pickup address.
+        :param end_latitude: Ending latitude or latitude of destination address.
+        :param end_longitude: Ending longitude or longitude of destination address.
+        :return: JSON
+        """
+        endpoint = 'estimates/price'
+        query_parameters = {
+            'start_latitude': start_latitude,
+            'start_longitude': start_longitude,
+            'end_latitude': end_latitude,
+            'end_longitude': end_longitude
+        }
+
+        return self.get_json(endpoint, 'GET', query_parameters, None, None)
+
+    def get_time_estimate(self, start_latitude, start_longitude, customer_uuid=None, product_id=None):
+        """
+        Get the ETA for Uber products.
+        :param start_latitude: Starting latitude.
+        :param start_longitude: Starting longitude.
+        :param customer_uuid: (Optional) Customer unique ID.
+        :param product_id: (Optional) If ETA is needed only for a specific product type.
+        :return: JSON
+        """
+
+        endpoint = 'estimates/time'
+        query_parameters = {
+                'start_latitude': start_latitude,
+                'start_longitude': start_longitude
+            }
+
+        if customer_uuid is not None:
+            query_parameters['customer_uuid'] = customer_uuid
+        elif product_id is not None:
+            query_parameters['product_id'] = product_id
+        elif customer_uuid is not None and product_id is not None:
+            query_parameters['customer_uuid'] = customer_uuid
+            query_parameters['product_id'] = product_id
+
+        return self.get_json(endpoint, 'GET', query_parameters, None, None)
